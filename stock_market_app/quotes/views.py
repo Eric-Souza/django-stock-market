2f615d629126e4725a1782e2bb9dccc46ab3b970
Eric Souza <eric.bh18souza@gmail.com>
2022-11-30 16:30:05 -0300

    feat: :sparkles: connected to iexcloud stock market api and made a get call

--- stock_market_app/quotes/views.py
+++ stock_market_app/quotes/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
 def home(request):
-  return render(request, 'home.html', {})
+  # iexcloud key: pk_29b131419c054b429aa9bf1f49b93cf6
+  import requests
+  import json
+  
+  api_request = requests.get(
+    "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_29b131419c054b429aa9bf1f49b93cf6"
+  )
+
+  try:
+    api = json.loads(api_request.content)
+  except Exception as e:
+    api = "Error..."
+
+  return render(request, 'home.html', {'api': api})
 
 
 def about(request):
