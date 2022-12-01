ea36d8e43b3ca5af35dd45ff7d4e87651949e48f
Eric Souza <eric.bh18souza@gmail.com>
2022-12-01 10:17:46 -0300

    feat: :sparkles: search field developed and stock model created

--- stock_market_app/quotes/views.py
+++ stock_market_app/quotes/views.py
@@ -2,20 +2,31 @@ from django.shortcuts import render
 
 def home(request):
   # iexcloud key: pk_29b131419c054b429aa9bf1f49b93cf6
+  
   import requests
   import json
   
-  api_request = requests.get(
-    "https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_29b131419c054b429aa9bf1f49b93cf6"
-  )
+  if request.method == 'POST':
+    ticker = request.POST['ticker']
+    
+    api_request = requests.get(
+      "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_29b131419c054b429aa9bf1f49b93cf6"
+    )
 
-  try:
-    api = json.loads(api_request.content)
-  except Exception as e:
-    api = "Error..."
+    try:
+      api = json.loads(api_request.content)
+    except Exception as e:
+      api = "Error..."
+      
+    return render(request, 'home.html', {'api': api})
+    
+  else: 
+    return render(request, 'home.html', {'ticker': 'Please enter a ticker symbol in the search input'})
 
-  return render(request, 'home.html', {'api': api})
-
-
+  
 def about(request):
   return render(request, 'about.html', {})
+
+
+def add_stock(request):
+  return render(request, 'add_stock.html', {})
